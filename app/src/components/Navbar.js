23a37a6f06de014df193428015ccfcb9c45235db
jordanfoote <jordan.foote@outlook.com>
2024-10-26 03:55:15 -0400

    Updated Navbar

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,28 +1,40 @@
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/logo.png";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="navbar">
       <nav>
         <Link className="logo" to="/">
-          <img src={logo} alt="SwiftTrim Logo" />
+          <div className="logo-container">
+            <img src={logo} alt="SwiftTrim Logo" className="logo-image" />
+          </div>{" "}
         </Link>
-        <div className="menu-items">
-          <NavLink exact="true" to="/">
+        <div className="hamburger" onClick={toggleMenu}>
+          &#9776; {/* Hamburger icon */}
+        </div>
+        <div className={`menu-items ${isMenuOpen ? "open" : ""}`}>
+          <NavLink exact to="/" onClick={toggleMenu}>
             <a>Home</a>
           </NavLink>
-          <NavLink to="/services">
+          <NavLink to="/services" onClick={toggleMenu}>
             <a>Services</a>
           </NavLink>
-          <NavLink to="/reviews">
+          <NavLink to="/reviews" onClick={toggleMenu}>
             <a>Reviews</a>
           </NavLink>
-          <NavLink to="/contact">
+          <NavLink to="/contact" onClick={toggleMenu}>
             <a>Contact</a>
           </NavLink>
-          <NavLink to="/quote">
+          <NavLink to="/quote" onClick={toggleMenu}>
             <a>Quote</a>
           </NavLink>
         </div>
